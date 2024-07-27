3a7a8304897e55149e62eeb96f721dc2d32a52e0
Cenk Yalçın <cekkooo09@gmail.com>
2024-07-27 18:08:47 +0300

    radio button type error fixed

--- components/RadioButton.vue
+++ components/RadioButton.vue
@@ -1,47 +1,52 @@
 <template>
-    <div class="form-group">
-      <label>{{ field.label }}</label>
-      <div v-for="(option, index) in field" :key="index" class="form-check">
-        <input
-          :type="field.form_field_type.type"
-          v-model="value"
-          :id="`${field.form_field_type_id}-${index}`"
-          :name="field.form_field_type_id"
-          :value="option.type"
-          
-          class="form-check-input"
-        />
-        <label :for="`${field.form_field_type_id}-${index}`" class="form-check-label">
-          {{ option.type }}
-        </label>
-      </div>
+  <div class="form-group">
+    <label :for="field.unique_id">{{ field.label }}</label>
+    <div v-for="(option, index) in field.form_field_options" :key="index" class="form-check">
+      <input
+        type="radio"
+        :id="`${field.unique_id}_${index}`"
+        :name="field.unique_id"
+        :value="option.option_value"
+        v-model="value"
+        :required="field.is_required"
+        class="form-check-input"
+      />
+      <label :for="`${field.unique_id}_${index}`" class="form-check-label">
+        {{ option.option_label }}
+      </label>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      field: {
-        type: Object,
-        required: true,
-      },
-      value: {
-        type: Object,
-        default: '',
-      },
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    field: {
+      type: Object,
+      required: true,
     },
-  };
-  </script>
-  
-  <style scoped>
-  .form-group {
-    margin-bottom: 15px;
-  }
-  .form-check {
-    margin-bottom: 10px;
-  }
-  .form-check-input {
-    margin-right: 10px;
-  }
-  </style>
-  
+    value: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-group {
+  margin-bottom: 15px;
+}
+
+.form-check {
+  margin-bottom: 10px;
+}
+
+.form-check-input {
+  margin-right: 10px;
+}
+
+.form-check-label {
+  margin-left: 5px;
+}
+</style>
